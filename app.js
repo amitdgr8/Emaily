5941e11e0c730c52eba475e91b929e2a2e60251c
amit kumar das <amit_peace@live.com>
2017-12-17 20:25:41 +0000

    changed res send message

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var app = express();
 
-app.get("/", (req, res)=>{
-    res.send({hi: 'amit'})
+app.get("/", function(req, res){
+    res.send("Hi From AMIT K D")
 })
 
 app.listen(process.env.PORT, process.env.IP)
