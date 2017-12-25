e2848425790975db7f751d7d0d8d53712d7ce955
amit kumar das <amit_peace@live.com>
2017-12-25 21:12:00 +0000

    updating production env

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var mongoose = require("mongoose");
+var cookieSession = require("cookie-session");
+var passport = require("passport");
 var keys = require("./config/keys");
 require("./models/users");
 require("./services/passport");
@@ -10,5 +12,13 @@ mongoose.connect(keys.mongoURI,{
 
 var app = express();
 
+app.use(
+    cookieSession({
+        maxAge: 10*24*60*60*1000,
+        keys:[keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 require("./routes/authRoutes")(app);
 app.listen(process.env.PORT, process.env.IP)
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,4 +7,13 @@ module.exports = app =>{
     );
     
     app.get("/auth/google/callback", passport.authenticate("google"));
+    
+    app.get("/api/logout", function(req, res){
+        req.logout();
+        res.send(req.user)
+    })
+    
+    app.get("/api/current_user", function(req, res){
+        res.send(req.user);
+    })
 };
