1669d4bd0ddf641a7ffe6bb4dc2518711cfb5acf
amit kumar das <amit_peace@live.com>
2017-12-25 15:31:26 +0000

    passport added. serialixe and deserialize user

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require("express");
+var mongoose = require("mongoose");
+var keys = require("./config/keys");
+require("./models/users");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI,{
+    useMongoClient: true
+});
+
 var app = express();
 
-app.get("/", function(req, res){
-    res.send("Hi From AMIT K D")
-})
-
+require("./routes/authRoutes")(app);
 app.listen(process.env.PORT, process.env.IP)
